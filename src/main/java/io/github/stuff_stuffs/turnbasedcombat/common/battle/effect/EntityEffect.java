f656065c431bcc55d870649b1d675a7dbd3a3b57
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-05-24 23:51:21 -0500

    oops

--- src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/effect/EntityEffect.java
+++ src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/effect/EntityEffect.java
@@ -5,8 +5,8 @@ import io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.EntityState;
 
 public interface EntityEffect {
     int BASE = 0;
-    int ADDITIVE = 1;
-    int MULTIPLY_BASE = 2;
+    int MULTIPLY_BASE = 1;
+    int ADDITIVE = 2;
     int MULTIPLY_TOTAL = 3;
 
     void tick(EntityState entityState, BattleStateView battleView);
