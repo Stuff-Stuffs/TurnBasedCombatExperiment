e80c39b2c419e89875025f721887a871c08c6d06
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-05-29 11:59:45 -0500

    BattleEquipment provides EntityAction

--- src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/entity/equipment/BattleEquipment.java
+++ src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/entity/equipment/BattleEquipment.java
@@ -1,11 +1,20 @@
 package io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.equipment;
 
 import io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.EntityState;
+import io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.EntityStateView;
+import io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.action.EntityAction;
 import io.github.stuff_stuffs.turnbasedcombat.common.battle.entity.inventory.BattleItem;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface BattleEquipment {
     BattleEquipmentType getType();
+
+    default List<EntityAction> getActions(final EntityStateView entityState) {
+        return Collections.emptyList();
+    }
 
     void initEvents(EntityState entityState);
 
