3ab71e89cfd89f48967516d2dd79fe059a0f1d65
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-06-09 23:45:04 -0500

    Switch reference map to object map

--- src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/event/EventMap.java
+++ src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/event/EventMap.java
@@ -1,6 +1,6 @@
 package io.github.stuff_stuffs.turnbasedcombat.common.battle.event;
 
-import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
@@ -10,16 +10,16 @@ public final class EventMap {
     private final Map<EventKey<?, ?>, EventHolder<?, ?>> eventMap;
 
     public EventMap() {
-        mutEventMap = new Reference2ObjectOpenHashMap<>();
-        eventMap = new Reference2ObjectOpenHashMap<>();
+        mutEventMap = new Object2ObjectOpenHashMap<>();
+        eventMap = new Object2ObjectOpenHashMap<>();
     }
 
-    public <T, V> void register(EventKey<T,V> key, final EventHolder<T, V> eventHolder) {
+    public <T, V> void register(final EventKey<T, V> key, final EventHolder<T, V> eventHolder) {
         if (eventMap.containsKey(key) || mutEventMap.containsKey(key)) {
             throw new RuntimeException("Duplicate event keys found!");
         }
-        if (eventHolder instanceof MutableEventHolder) {
-            mutEventMap.put(key, (MutableEventHolder<?, ?>) eventHolder);
+        if (eventHolder instanceof MutableEventHolder<?, ?> mut) {
+            mutEventMap.put(key, mut);
         } else {
             eventMap.put(key, eventHolder);
         }
