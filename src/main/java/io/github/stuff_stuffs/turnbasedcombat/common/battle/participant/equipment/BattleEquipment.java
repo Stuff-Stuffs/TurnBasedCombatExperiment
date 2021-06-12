76c3183d9eca0d60cb3e9d130a14b184aa65c6f8
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-06-12 00:02:15 -0500

    Provide state to getActions

--- src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/participant/equipment/BattleEquipment.java
+++ src/main/java/io/github/stuff_stuffs/turnbasedcombat/common/battle/participant/equipment/BattleEquipment.java
@@ -1,13 +1,15 @@
 package io.github.stuff_stuffs.turnbasedcombat.common.battle.participant.equipment;
 
+import io.github.stuff_stuffs.turnbasedcombat.common.battle.BattleStateView;
 import io.github.stuff_stuffs.turnbasedcombat.common.battle.participant.BattleParticipantState;
+import io.github.stuff_stuffs.turnbasedcombat.common.battle.participant.BattleParticipantStateView;
 import io.github.stuff_stuffs.turnbasedcombat.common.battle.participant.action.ParticipantAction;
 
 import java.util.Collections;
 import java.util.List;
 
 public interface BattleEquipment {
-    default List<? extends ParticipantAction> getActions() {
+    default List<? extends ParticipantAction> getActions(BattleStateView stateView, BattleParticipantStateView participantView) {
         return Collections.emptyList();
     }
 
