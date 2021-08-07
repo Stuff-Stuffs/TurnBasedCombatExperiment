246e0b2bcc0c24c7c0132c2e48d189a2d3b0404f
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-08-07 15:51:47 -0500

    Start of testmod

---
+++ test/src/main/java/io/github/stuff_stuffs/tbcextest/client/TestClient.java
@@ -0,0 +1,10 @@
+package io.github.stuff_stuffs.tbcextest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ test/src/main/java/io/github/stuff_stuffs/tbcextest/common/Test.java
@@ -0,0 +1,10 @@
+package io.github.stuff_stuffs.tbcextest.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Test implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
