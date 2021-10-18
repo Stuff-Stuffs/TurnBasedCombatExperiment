36ebb09e22f0169f273e7de7bb03e791ca73b06b
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-10-18 12:08:13 -0500

    Fabric bugfix for real this time

--- util/src/main/java/io/github/stuff_stuffs/tbcexutil/mixin/MixinKeyBindingRegistryImpl.java
+++ util/src/main/java/io/github/stuff_stuffs/tbcexutil/mixin/MixinKeyBindingRegistryImpl.java
@@ -1,6 +1,7 @@
 package io.github.stuff_stuffs.tbcexutil.mixin;
 
 import com.google.common.collect.Lists;
+import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
 import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
 import net.minecraft.client.option.KeyBinding;
 import org.spongepowered.asm.mixin.Final;
@@ -12,23 +13,18 @@ import java.util.List;
 
 @Mixin(KeyBindingRegistryImpl.class)
 public class MixinKeyBindingRegistryImpl {
-    @Shadow @Final private static List<KeyBinding> moddedKeyBindings;
+    @Shadow
+    @Final
+    private static List<KeyBinding> moddedKeyBindings;
 
     /**
      * @author Stuff-Stuffs
      * @reason https://github.com/FabricMC/fabric/issues/1772
      */
     @Overwrite
-    public static KeyBinding[] process(KeyBinding[] keysAll) {
-        List<KeyBinding> newKeysAll = Lists.newArrayList(keysAll);
-        newKeysAll.removeIf(key -> {
-            for (KeyBinding binding : moddedKeyBindings) {
-                if(key.getTranslationKey().equals(binding.getTranslationKey())) {
-                    return true;
-                }
-            }
-            return false;
-        });
+    public static KeyBinding[] process(final KeyBinding[] keysAll) {
+        final List<KeyBinding> newKeysAll = Lists.newArrayList(keysAll);
+        newKeysAll.removeAll(new ReferenceArrayList<>(moddedKeyBindings));
         newKeysAll.addAll(moddedKeyBindings);
         return newKeysAll.toArray(new KeyBinding[0]);
     }
