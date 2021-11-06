4820ed3818fe8a5c2afd3eb61bc8814d32cb26c9
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-11-06 14:00:43 -0500

    More material tags

--- content/equipment/src/main/java/io/github/stuff_stuffs/tbcexequipment/common/material/MaterialTags.java
+++ content/equipment/src/main/java/io/github/stuff_stuffs/tbcexequipment/common/material/MaterialTags.java
@@ -9,6 +9,10 @@ import net.minecraft.tag.Tag;
 public final class MaterialTags {
     public static final RequiredTagList<Material> REQUIRED_TAG_LIST = RequiredTagListRegistry.register(Materials.REGISTRY_KEY, "tags/tbcex/materials");
     public static final Tag.Identified<Material> BLADE_MATERIALS = TagRegistry.create(TBCExEquipment.createId("blade_materials"), REQUIRED_TAG_LIST::getGroup);
+    public static final Tag.Identified<Material> SIMPLE_POMMEL_MATERIALS = TagRegistry.create(TBCExEquipment.createId("simple_pommel_materials"), REQUIRED_TAG_LIST::getGroup);
+    public static final Tag.Identified<Material> HANDLE_MATERIALS = TagRegistry.create(TBCExEquipment.createId("handle_materials"), REQUIRED_TAG_LIST::getGroup);
+    public static final Tag.Identified<Material> SIMPLE_SWORD_GUARD_MATERIALS = TagRegistry.create(TBCExEquipment.createId("simple_sword_guard_materials"), REQUIRED_TAG_LIST::getGroup);
+    public static final Tag.Identified<Material> FANCY_SWORD_GUARD_MATERIALS = TagRegistry.create(TBCExEquipment.createId("fancy_sword_guard_materials"), REQUIRED_TAG_LIST::getGroup);
 
     public static void init() {
     }
--- content/equipment/src/main/java/io/github/stuff_stuffs/tbcexequipment/common/part/Parts.java
+++ content/equipment/src/main/java/io/github/stuff_stuffs/tbcexequipment/common/part/Parts.java
@@ -15,11 +15,11 @@ import java.util.List;
 public final class Parts {
     public static final RegistryKey<Registry<Part<?>>> REGISTRY_KEY = RegistryKey.ofRegistry(TBCExEquipment.createId("parts"));
     public static final Registry<Part<?>> REGISTRY = FabricRegistryBuilder.from(new SimpleRegistry<>(REGISTRY_KEY, Lifecycle.stable())).buildAndRegister();
-    public static final Part<HandlePartData> HANDLE_PART = new Part<>(new LiteralText("Handle"), List.of(new LiteralText("A basic handle")), material -> true, HandlePartData.CODEC, HandlePartData::new);
+    public static final Part<HandlePartData> HANDLE_PART = new Part<>(new LiteralText("Handle"), List.of(new LiteralText("A basic handle")), MaterialTags.HANDLE_MATERIALS::contains, HandlePartData.CODEC, HandlePartData::new);
     public static final Part<SwordBladePartData> SWORD_BLADE_PART = new Part<>(new LiteralText("Sword Blade"), List.of(new LiteralText("A basic sword blade")), MaterialTags.BLADE_MATERIALS::contains, SwordBladePartData.CODEC, SwordBladePartData::new);
-    public static final Part<SimpleSwordGuardPartData> SIMPLE_SWORD_GUARD_PART = new Part<>(new LiteralText("Simple Sword Guard"), List.of(new LiteralText("A simple sword guard")), material -> true, SimpleSwordGuardPartData.CODEC, SimpleSwordGuardPartData::new);
-    public static final Part<FancySwordGuardPartData> FANCY_SWORD_GUARD = new Part<>(new LiteralText("Fancy Sword Guard"), List.of(new LiteralText("A fancy sword guard")), material -> true, FancySwordGuardPartData.CODEC, FancySwordGuardPartData::new);
-    public static final Part<SimplePommelPartData> SIMPLE_POMMEL = new Part<>(new LiteralText("Simple Pommel"), List.of(new LiteralText("A simple pommel")), material -> true, SimplePommelPartData.CODEC, SimplePommelPartData::new);
+    public static final Part<SimpleSwordGuardPartData> SIMPLE_SWORD_GUARD_PART = new Part<>(new LiteralText("Simple Sword Guard"), List.of(new LiteralText("A simple sword guard")), MaterialTags.SIMPLE_SWORD_GUARD_MATERIALS::contains, SimpleSwordGuardPartData.CODEC, SimpleSwordGuardPartData::new);
+    public static final Part<FancySwordGuardPartData> FANCY_SWORD_GUARD = new Part<>(new LiteralText("Fancy Sword Guard"), List.of(new LiteralText("A fancy sword guard")), MaterialTags.FANCY_SWORD_GUARD_MATERIALS::contains, FancySwordGuardPartData.CODEC, FancySwordGuardPartData::new);
+    public static final Part<SimplePommelPartData> SIMPLE_POMMEL = new Part<>(new LiteralText("Simple Pommel"), List.of(new LiteralText("A simple pommel")), MaterialTags.SIMPLE_POMMEL_MATERIALS::contains, SimplePommelPartData.CODEC, SimplePommelPartData::new);
 
     public static void init() {
         Registry.register(REGISTRY, TBCExEquipment.createId("handle"), HANDLE_PART);
