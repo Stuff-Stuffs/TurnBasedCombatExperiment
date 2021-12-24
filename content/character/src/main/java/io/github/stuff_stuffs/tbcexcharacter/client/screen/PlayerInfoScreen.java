c5ed8a1c0863cc2359706ff9e7cfeff541506fc1
Stuff-Stuffs <stevenziehlke@gmail.com>
2021-12-24 15:20:36 -0600

    StackWidget

--- content/character/src/main/java/io/github/stuff_stuffs/tbcexcharacter/client/screen/PlayerInfoScreen.java
+++ content/character/src/main/java/io/github/stuff_stuffs/tbcexcharacter/client/screen/PlayerInfoScreen.java
@@ -1,32 +1,34 @@
 package io.github.stuff_stuffs.tbcexcharacter.client.screen;
 
 import io.github.stuff_stuffs.tbcexcharacter.client.screen.widget.PlayerStatsWidget;
-import io.github.stuff_stuffs.tbcexcharacter.common.entity.CharacterInfo;
-import io.github.stuff_stuffs.tbcexcharacter.mixin.api.PlayerCharacterInfoSupplier;
+import io.github.stuff_stuffs.tbcexcore.client.gui.widget.BattleInventoryFilterWidget;
 import io.github.stuff_stuffs.tbcexgui.client.screen.TBCExScreen;
 import io.github.stuff_stuffs.tbcexgui.client.widget.ParentWidget;
+import io.github.stuff_stuffs.tbcexgui.client.widget.StackWidget;
 import io.github.stuff_stuffs.tbcexgui.client.widget.WidgetPosition;
+import io.github.stuff_stuffs.tbcexgui.client.widget.interaction.SelectionWheelWidget;
+import io.github.stuff_stuffs.tbcexgui.client.widget.panel.BasicPanelWidget;
 import io.github.stuff_stuffs.tbcexgui.client.widget.panel.RootPanelWidget;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class PlayerInfoScreen extends TBCExScreen {
     public PlayerInfoScreen() {
         super(new LiteralText("PlayerInfo"), new RootPanelWidget());
         final ParentWidget widget = (ParentWidget) this.widget;
-        final double edgeStart = 0.015625;
-        final double width = 1 - 2 * edgeStart;
-        //final TabsWidget tabsWidget = new TabsWidget(WidgetPosition.of(1 - edgeStart - 0.015 + 0.0375, edgeStart - 0.015, 1), TabsWidget.Side.RIGHT, 0.125);
-        //widget.addWidget(tabsWidget);
-        //tabsWidget.addPanel("main", new LiteralText("Character View"), width + 0.03, width + 0.03, new Identifier("tbcexgui", "gui/transparent"));
-        //final ParentWidget mainPanel = tabsWidget.getPanel("main");
-        //final PlayerStatsWidget statsWidget = new PlayerStatsWidget(WidgetPosition.of(edgeStart + width / 2, edgeStart + width / 2, 1), width / 2, width / 2, 0.0625, MinecraftClient.getInstance().player);
-        //mainPanel.addWidget(statsWidget);
-        //final CharacterInfo characterInfo = ((PlayerCharacterInfoSupplier) MinecraftClient.getInstance().player).tbcex_getCharacterInfo();
-        //if (characterInfo.getCurrentLevelProgress() >= 1) {
-        //    tabsWidget.addPanel("level_up", new LiteralText("Level Up"), width + 0.03, width + 0.03, new Identifier("tbcexgui", "gui/transparent"));
-        //    final ParentWidget levelUpPanel = tabsWidget.getPanel("level_up");
-        //}
+        final WidgetPosition panelPos = WidgetPosition.of(0.5, 0.5, 1);
+        final StackWidget stackWidget = new StackWidget();
+        final BasicPanelWidget characterInfoPanel = new BasicPanelWidget(WidgetPosition.of(0.125, 0.125, 1), 0.75, 0.75);
+        final PlayerStatsWidget statsWidget = new PlayerStatsWidget(WidgetPosition.of(0.625, 0.125, 1), 0.375, 0.375, 0.0625, MinecraftClient.getInstance().player);
+        characterInfoPanel.addWidget(statsWidget);
+        //TODO level up button
+        final SelectionWheelWidget selection = SelectionWheelWidget.builder().addEntry(
+                BattleInventoryFilterWidget.FIRST_BACKGROUND_COLOUR, 127, BattleInventoryFilterWidget.SECOND_BACKGROUND_COLOUR, 127,
+                () -> new LiteralText("Character Info"), List.of(), () -> true, () -> stackWidget.push(characterInfoPanel)
+        ).build(0.15, 0.3, 0.3125, panelPos);
+        stackWidget.setFallback(selection);
+        widget.addWidget(stackWidget);
     }
 }
